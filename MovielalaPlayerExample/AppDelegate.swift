79e3fe1ebc97fe96c9e3470c4ad4925f32de7a2c
Toygar Dündaralp <tdundaralp@gmail.com>
2015-06-04 15:37:54 -0700

    Updated NSURL > pathForResource

--- MovielalaPlayerExample/AppDelegate.swift
+++ MovielalaPlayerExample/AppDelegate.swift
@@ -11,20 +11,19 @@ import MovielalaPlayer
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+  
   var window: UIWindow?
-
+  
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     
-    // Video Test URLs
-    // http://ia700409.us.archive.org/15/items/END.CIV-DVDrip/ENDCIV.m4v // Long Video
-    // http://jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v // Short Video
-    
-    //let playerVC = MovielalaPlayerViewController(contentURL: NSURL(string: "http://player.vimeo.com/external/129470313.m3u8?p=high,standard,mobile&s=87cf536853be9d543e30f9e227285906")!)
-    
+    var skinFile:NSURL = NSURL(
+      fileURLWithPath: NSBundle.mainBundle().pathForResource(
+        "SampleSkinFile",
+        ofType: "json")!
+      )!
     let playerVC = MovielalaPlayerViewController(
       contentURL: NSURL(string: "http://player.vimeo.com/external/129470313.m3u8?p=high,standard,mobile&s=87cf536853be9d543e30f9e227285906")!,
-      configFileURL: NSURL(string: "SampleSkinFile")!
+      configFileURL: skinFile
     )
     playerVC.title = "Jurrasic World"
     self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
@@ -34,29 +33,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     return true
   }
-
+  
   func applicationWillResignActive(application: UIApplication) {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
   }
-
+  
   func applicationDidEnterBackground(application: UIApplication) {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
   }
-
+  
   func applicationWillEnterForeground(application: UIApplication) {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
   }
-
+  
   func applicationDidBecomeActive(application: UIApplication) {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
   }
-
+  
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
-
-
+  
+  
 }
 
