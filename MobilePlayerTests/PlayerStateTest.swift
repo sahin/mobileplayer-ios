c3909804ebf9f9d684dda5ba5bebe019e2366084
Toygar Dündaralp <tdundaralp@gmail.com>
2015-08-05 17:53:30 -0700

    Add player state play/pause testcase

---
+++ MobilePlayerTests/PlayerStateTest.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerStateTest.swift
+//  MobilePlayer
+//
+//  Created by Toygar Dündaralp on 8/5/15.
+//  Copyright (c) 2015 MovieLaLa. All rights reserved.
+//
+
+import UIKit
+
+class PlayerStateTest: XCTestCase {
+   
+}
