309aaf00960c3f24d44cd76ecf462950db7c307a
Baris Sencan <baris.sncn@gmail.com>
2015-12-09 18:26:26 -0800

    [WatermarkViewController] Add 8 pt margin to watermarks

--- MobilePlayer/WatermarkViewController.swift
+++ MobilePlayer/WatermarkViewController.swift
@@ -34,28 +34,28 @@ class WatermarkViewController: MobilePlayerOverlayViewController {
       watermarkImageView.frame.origin.y = (size.height - watermarkSize.height) / 2
     case .Top:
       watermarkImageView.frame.origin.x = (size.width - watermarkSize.width) / 2
-      watermarkImageView.frame.origin.y = 0
+      watermarkImageView.frame.origin.y = 8
     case .TopRight:
-      watermarkImageView.frame.origin.x = size.width - watermarkSize.width
-      watermarkImageView.frame.origin.y = 0
+      watermarkImageView.frame.origin.x = size.width - watermarkSize.width - 8
+      watermarkImageView.frame.origin.y = 8
     case .Right:
-      watermarkImageView.frame.origin.x = size.width - watermarkSize.width
+      watermarkImageView.frame.origin.x = size.width - watermarkSize.width - 8
       watermarkImageView.frame.origin.y = (size.height - watermarkSize.height) / 2
     case .BottomRight:
-      watermarkImageView.frame.origin.x = size.width - watermarkSize.width
-      watermarkImageView.frame.origin.y = size.height - watermarkSize.height
+      watermarkImageView.frame.origin.x = size.width - watermarkSize.width - 8
+      watermarkImageView.frame.origin.y = size.height - watermarkSize.height - 8
     case .Bottom:
       watermarkImageView.frame.origin.x = (size.width - watermarkSize.width) / 2
-      watermarkImageView.frame.origin.y = size.height - watermarkSize.height
+      watermarkImageView.frame.origin.y = size.height - watermarkSize.height - 8
     case .BottomLeft:
-      watermarkImageView.frame.origin.x = 0
-      watermarkImageView.frame.origin.y = size.height - watermarkSize.height
+      watermarkImageView.frame.origin.x = 8
+      watermarkImageView.frame.origin.y = size.height - watermarkSize.height - 8
     case .Left:
-      watermarkImageView.frame.origin.x = 0
+      watermarkImageView.frame.origin.x = 8
       watermarkImageView.frame.origin.y = (size.height - watermarkSize.height) / 2
     case .TopLeft:
-      watermarkImageView.frame.origin.x = 0
-      watermarkImageView.frame.origin.y = 0
+      watermarkImageView.frame.origin.x = 8
+      watermarkImageView.frame.origin.y = 8
     }
   }
 }
